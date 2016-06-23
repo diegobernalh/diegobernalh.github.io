0854989ea76b89e7826b3589ddf9539ddbea8a78
diegobernalh <diegobernalh@yahoo.com>
2016-06-23 12:51:55 +0200

    more changes

--- CSS/javascript_instagram.js
+++ CSS/javascript_instagram.js
@@ -1,68 +1,39 @@
+/*
 window.onload = addListeners();
-
-function addListeners() {
-    document.getElementById('draginstagram').addEventListener('mousedown', mouseDown, false);
-    window.addEventListener('mouseup', mouseUp, false);
-
-}
-
-function mouseUp() {
-    window.removeEventListener('mousemove', divMove, true);
-}
-
-function mouseDown(e) {
-    window.addEventListener('mousemove', divMove, true);
-}
-
-function divMove(e) {
-    var div = document.getElementById('draginstagram');
-    div.style.position = 'absolute';
-    div.style.top = e.clientY + 'px';
-    div.style.left = e.clientX + 'px';
-}
-
-window.onload = addListeners();
-
-var button1 = document.getElementById('dragemail');
 
 
 function addListeners() {
     document.getElementById('dragemail').addEventListener('mousedown', mouseDown, false);
+
+
     window.addEventListener('mouseup', mouseUp, false);
-
 }
 
-function mouseUp() {
-    window.removeEventListener('mousemove', divMove, true);
+function divMove(e) {
+    var div = document.getElementById('dragemail');
+    div.style.top = e.clientY - (div.offsetHeight / 2) + 'px';
+    div.style.left = e.clientX - (div.offsetWidth / 2) + 'px';
+
+    console.log(e.srcElement)
 }
+
 
 function mouseDown(e) {
     window.addEventListener('mousemove', divMove, true);
 }
 
-function divMove(e, div) {
-
-    var div = document.getElementById('dragemail');
-
-    // div.style.position = 'absolute';
-
-
-    var x = e.clientX; // Get the horizontal coordinate
-    var y = e.clientY; // Get the vertical coordinate
-
-    var coor = "X coords: " + x + ", Y coords: " + y;
-
-    console.log(div.offsetHeight);
-
-
-
-    div.style.top = e.clientY - (div.offsetHeight / 2) + 'px';
-    div.style.left = e.clientX - (div.offsetWidth / 2) + 'px';
-
+function mouseUp() {
+    window.removeEventListener('mousemove', divMove, true);
 }
+*/
 
-
-
+$(function () {
+    $("#dragemail").draggable();
+    $("#draginstagram").draggable();
+    $("#dragbehance").draggable();
+    $("#dragpepepino").draggable();
+    $("#dragticket").draggable();
+});
 
 
 
